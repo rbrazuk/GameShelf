33c7a73436d7e4a2b9b05cade2c57117ab97b0c8
rbrazuk <ross.brazuk@gmail.com>
2016-11-14 11:29:55 -0500

    added list item layout for game

--- app/src/main/java/com/example/ross/gameshelf/activities/GameListActivity.java
+++ app/src/main/java/com/example/ross/gameshelf/activities/GameListActivity.java
@@ -2,14 +2,22 @@ package com.example.ross.gameshelf.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.ross.gameshelf.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class GameListActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_games) RecyclerView mGamesRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+
+        ButterKnife.bind(this);
     }
 }
