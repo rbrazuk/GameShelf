0d99cf730f531a51a6b4c6f0eef15ecdce67c1c9
rbrazuk <ross.brazuk@gmail.com>
2016-11-14 10:52:38 -0500

    Added GameListActivity

--- app/src/main/java/com/example/ross/gameshelf/activities/MainActivity.java
+++ app/src/main/java/com/example/ross/gameshelf/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ross.gameshelf;
+package com.example.ross.gameshelf.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ross.gameshelf.R;
 
 public class MainActivity extends AppCompatActivity {
 
